2458fb3c4ce294320d4d4c27234aa1d3a88a9736
pippocrespi <pippocrespi@gmail.com>
2025-11-16 10:46:30 +0100

    sw

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// REGISTRAZIONE SERVICE WORKER
+// ==========================================================
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registrato con successo:', registration);
+      })
+      .catch(error => {
+        console.log('Registrazione Service Worker fallita:', error);
+      });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('checklist-form');
     const statusDiv = document.getElementById('status');

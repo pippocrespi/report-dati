c670797e372fbe7985cf3193f8048c08b7b1a986
pippocrespi <pippocrespi@gmail.com>
2025-11-17 20:53:58 +0100

    gg

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const fotoInput = document.getElementById('foto'); // Prendiamo l'input della foto
 
     // INCOLLA IL TUO NUOVO URL DI DEPLOY QUI
-    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwRrheaQhOzTz_YwPTEL_zJDCGD9T9qmPDhuvHCOnDG5uz1wp85y1NNLvYNhXZH65Dc/exec';
+    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzVo1mX1URbfT-8t8Au0u_8LGHlI649PL-Z0bT04xguEwESJPC8KqNYtAS57MBKPB2O/exec';
 
     function updateStatus(message, type) {
         statusDiv.textContent = message;
